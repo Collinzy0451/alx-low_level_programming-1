f2f2bcc0590a0a31a26e173fe883313625562db2
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-11 18:13:41 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return 0 (success)
+ */
+
+int main(void)
+{
+char t;
+t = 'a';
+while 
+(t <= 'z')
+{
+	putchar(t);
+	t++
+}
+puchar('\n');
+return (0);
+}
