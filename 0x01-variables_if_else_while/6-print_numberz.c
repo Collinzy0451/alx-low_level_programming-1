e382e97af3eb1fc67b61ef56ab2c36bb080c2a47
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-14 01:07:51 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int y;
+
+	y = 0;
+	while
+		(y < 10)
+		{
+			putchar(y + '0');
+y++;
+		}
+	putchar('\n');
+	return (0);
+}
