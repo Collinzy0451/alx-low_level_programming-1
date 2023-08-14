79c0a62a467845c684238b567286da64ae11cae3
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-13 17:46:21 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		printf("%d", y);
+	}
+putchar('\n');
+return (0);
+}
