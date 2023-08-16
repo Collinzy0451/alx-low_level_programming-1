885235c47f5fdaa19015bfc4c0893c0dd589889f
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-16 01:57:31 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,20 +5,20 @@
  * Return: 0 (success)
  */
 
-void print_alphabet_x10(void)
-{
-	char c;
+void print_alphabet_x10(void);
+	char letter;
+
 	int i;
 
-	i = 0;
-	while
-		(i < 10)
-		{
-			for (c = 'a'; c < 'z'; c++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
-			i++;
-		}
-}
+	int i = 0;
+
+	(while i < 10; i++)
+	{
+		letter = 'a';
+	}
+	(while letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
