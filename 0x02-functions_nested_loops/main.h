6a54afdace22c50d47fe80f19c8f94cf78bd55f5
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-16 00:33:13 -0700

    main.h commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,17 @@
 #ifndef MAN_HEADER_FILE
 #define MAN_HEADER_FILE
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int add(int, int);
+
 #endif
