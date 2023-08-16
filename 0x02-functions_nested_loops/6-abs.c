413888a9428fd03bb030f2d9972109bc43d90878
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-16 14:24:27 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @c: The number to be computed.
+ * Return: Absolute value of number or zero
+ */
+int _abs(int)
+
+{
+	int i;
+
+	if (i => 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
