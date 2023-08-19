90341348d71bab3e6ac27eb672a1ade4ef6c233a
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-08-18 17:50:59 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - prints the largest prime num
  * Return: 0
@@ -7,20 +7,20 @@ int main(void)
 {
 	long prime = 612852475143, devisor;
 
-	while (divisor < (prime / 2))
+	while (devisor < (prime / 2))
 	{
 		if ((prime % 2) == 0)
+		
 		{
 			prime /= 2;
 				continue;
 		}
-		for (divisor = 3; divisor < (prime / 2)); divosor + = 2
+		for (devisor = 3; devisor < (prime / 2); devisor += 2)
 		{
-			if ((prime % divisor) == 0)
-				prime /= divisor;
+			if ((prime % devisor) == 0)
+				prime /= devisor;
 		}
 	}
 	printf("%ld\n", prime);
 	return (0);
-
 }
